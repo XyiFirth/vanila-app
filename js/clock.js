837f40d58b4ac363c5223df754968f30c8303ca7
XyiFirth <99185572+XyiFirth@users.noreply.github.com>
2022-03-11 00:03:39 +0900

    Clock_add

--- js/clock.js
+++ js/clock.js
@@ -1,3 +1,11 @@
 const clock = document.querySelector("h2#clock");
 
-clock.innerindex = "lalalal"
+function getClock() {
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText = (`${hours}:${minutes}:${seconds}`)
+}
+getClock(); //첫 인터벌을 가지고 오는 1초를 당겨오기 위해 들어오자마자 보여주는 작업
+setInterval(getClock, 1000);
